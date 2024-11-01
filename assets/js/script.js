f70a152f6f88c864836fb92c8e5280a40582d49b
Flávio Oliveira <foliveiradark@gmail.com>
2024-11-01 14:14:43 -0300

    feat: inclui novas secões

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,30 @@ $(window).on('scroll', function () {
         $('.navbar').removeClass('sticky-top shadow-sm');
     }
 });
+
+//SWIPER JS
+let swiperCards = new Swiper('.card__content', {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints:{
+        600:{
+            slidesPerView:2,
+        },
+        968:{
+            slidesPerView:3,
+        },
+    },
+});
